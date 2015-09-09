02b57c2c3d98f71e2c8b6d6f2f3f9f6f61abaac3
Christian W <christian_wells@esri.com>
2015-09-09 11:33:18 -0700

    Update IMPDP.sql

--- plsql/data-pump/IMPDP.sql
+++ plsql/data-pump/IMPDP.sql
@@ -14,6 +14,7 @@ be run one step at a time.
 History:
 
 Christian Wells        11/03/2014               Original coding.
+Tina Morgan           12/01/2014
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -49,7 +50,6 @@ GRANT EXECUTE ON dbms_lock TO public;
 GRANT EXECUTE ON dbms_lob TO public;
 GRANT EXECUTE ON dbms_utility TO public;
 GRANT EXECUTE ON dbms_sql TO public;
-GRANT EXECUTE ON utl_raw TO public;
 
 
 --4. Create all required tablespaces
